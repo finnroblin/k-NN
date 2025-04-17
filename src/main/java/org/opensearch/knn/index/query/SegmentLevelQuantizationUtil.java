da281f5d7af59596d66d6df133a7be94cda3e3ab
Finn Roblin <finnrobl@amazon.com>
2025-04-16 17:13:18 -0700

    fixing some things
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>

--- src/main/java/org/opensearch/knn/index/query/SegmentLevelQuantizationUtil.java
+++ src/main/java/org/opensearch/knn/index/query/SegmentLevelQuantizationUtil.java
@@ -26,7 +26,7 @@ public class SegmentLevelQuantizationUtil {
 
     public static boolean isAdcEnabled(SegmentLevelQuantizationInfo segmentLevelQuantizationInfo) {
         // return true;
-        // // return false;
+        // return false;
         return segmentLevelQuantizationInfo != null
             && ScalarQuantizationParams.generateTypeIdentifier(ScalarQuantizationType.ONE_BIT)
                 .equals(segmentLevelQuantizationInfo.getQuantizationParams().getTypeIdentifier());
