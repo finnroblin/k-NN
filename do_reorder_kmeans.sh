7021b2fa0a133c8416565be77bf6fc6ed433874a
Finn Roblin <finnrobl@amazon.com>
2026-02-04 13:18:03 -0800

    fix libraryt path for kmeans
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>

--- do_reorder_kmeans.sh
+++ do_reorder_kmeans.sh
@@ -3,6 +3,6 @@
 cp /home/ec2-user/k-NN/jni/build/release/*.so \
    /home/ec2-user/k-NN/build/testclusters/integTest-0/distro/3.5.0-ARCHIVE/plugins/opensearch-knn/
 
-java -cp \
-        "/home/ec2-user/k-NN/build/testclusters/integTest-0/distro/3.5.0-ARCHIVE/lib/*:/home/ec2-user/k-NN/build/testclusters/integTest-0/distro/3.5.0-ARCHIVE/plugins/opensearch-knn/*" \
-          org.opensearch.knn.memoryoptsearch.faiss.reorder.ReorderAllWithKMeans
+java -Djava.library.path="/home/ec2-user/k-NN/build/testclusters/integTest-0/distro/3.5.0-ARCHIVE/plugins/opensearch-knn" \
+    -cp "/home/ec2-user/k-NN/build/testclusters/integTest-0/distro/3.5.0-ARCHIVE/lib/*:/home/ec2-user/k-NN/build/testclusters/integTest-0/distro/3.5.0-ARCHIVE/plugins/opensearch-knn/*" \
+    org.opensearch.knn.memoryoptsearch.faiss.reorder.ReorderAllWithKMeans
