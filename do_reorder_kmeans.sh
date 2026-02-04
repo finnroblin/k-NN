b4392464ed5602e7a86e4e83f2cd5e2db06353d4
Finn Roblin <finnrobl@amazon.com>
2026-02-04 13:16:07 -0800

    chmod scripts, add native lib copy for kmeans
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>

--- do_reorder_kmeans.sh
+++ do_reorder_kmeans.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cp /home/ec2-user/k-NN/jni/build/release/*.so \
+   /home/ec2-user/k-NN/build/testclusters/integTest-0/distro/3.5.0-ARCHIVE/plugins/opensearch-knn/
+
 java -cp \
         "/home/ec2-user/k-NN/build/testclusters/integTest-0/distro/3.5.0-ARCHIVE/lib/*:/home/ec2-user/k-NN/build/testclusters/integTest-0/distro/3.5.0-ARCHIVE/plugins/opensearch-knn/*" \
           org.opensearch.knn.memoryoptsearch.faiss.reorder.ReorderAllWithKMeans
