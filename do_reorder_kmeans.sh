be999849a9bdfd91bcaa07abe28bab3329a9cff8
Finn Roblin <finnrobl@amazon.com>
2026-03-02 11:32:06 -0800

    fix SNAPSHOT -> ARCHIVE
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>

--- do_reorder_kmeans.sh
+++ do_reorder_kmeans.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cp /home/ec2-user/k-NN-finn/jni/build/release/*.so \
-   /home/ec2-user/k-NN-finn/build/testclusters/integTest-0/distro/3.6.0-SNAPSHOT/plugins/opensearch-knn/
+   /home/ec2-user/k-NN-finn/build/testclusters/integTest-0/distro/3.6.0-ARCHIVE/plugins/opensearch-knn/
 
-java -Djava.library.path="/home/ec2-user/k-NN-finn/build/testclusters/integTest-0/distro/3.6.0-SNAPSHOT/plugins/opensearch-knn" \
-    -cp "/home/ec2-user/k-NN-finn/build/testclusters/integTest-0/distro/3.6.0-SNAPSHOT/lib/*:/home/ec2-user/k-NN-finn/build/testclusters/integTest-0/distro/3.6.0-SNAPSHOT/plugins/opensearch-knn/*" \
+java -Djava.library.path="/home/ec2-user/k-NN-finn/build/testclusters/integTest-0/distro/3.6.0-ARCHIVE/plugins/opensearch-knn" \
+    -cp "/home/ec2-user/k-NN-finn/build/testclusters/integTest-0/distro/3.6.0-ARCHIVE/lib/*:/home/ec2-user/k-NN-finn/build/testclusters/integTest-0/distro/3.6.0-ARCHIVE/plugins/opensearch-knn/*" \
     org.opensearch.knn.memoryoptsearch.faiss.reorder.ReorderAllWithKMeans
